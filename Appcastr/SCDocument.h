5036df862558f51a35f38d84c0b1606ef9096462
Alex Jackson <alex@alexj.me>
2012-06-07 13:49:12 +0100

    Initial Commit

---
+++ Appcastr/SCDocument.h
@@ -0,0 +1,13 @@
+//
+//  SCDocument.h
+//  Appcastr
+//
+//  Created by Alex Jackson on 07/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SCDocument : NSDocument
+
+@end
