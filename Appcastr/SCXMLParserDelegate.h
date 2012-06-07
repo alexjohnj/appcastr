1c238106278d3f35f967df895739301f18989026
Alex Jackson <alex@alexj.me>
2012-06-07 14:15:01 +0100

    Created a NSXMLParser delegate

---
+++ Appcastr/SCXMLParserDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SCXMLParserDelegate.h
+//  Appcastr
+//
+//  Created by Alex Jackson on 07/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCXMLParserDelegate : NSObject <NSXMLParserDelegate>
+
+@end
